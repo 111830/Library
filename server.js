07a11fc3ee7b2247b29d2b143c3bd08449dabca4
111830 <139061573+111830@users.noreply.github.com>
2025-07-07 23:32:56 +0200

    Shtimi

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const { Pool } = require('pg');
 const app = express();
 const port = 3000;
 
+
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'libraria_db',
-    password: 'Renis1234$',
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 app.use(express.static(path.join(__dirname)));
